819f294cac7bc10afd2455db674951b36c095248
isoldkidsonebay <itzmahir24@gmail.com>
2026-01-20 16:12:41 -0500

    I HATE NET.MINECRAFT.SERVER

---
+++ libs/src/main/java/me/isoldkidsonebay/libs/Libs.java
@@ -0,0 +1,17 @@
+package me.isoldkidsonebay.libs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Libs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
